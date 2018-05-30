6b11c5df43e35c061cd68dbc441f978ac5d80dd0
braye98 <39682568+braye98@users.noreply.github.com>
2018-05-30 10:44:53 -0400

    Create ejercicio2.rb

---
+++ ejercicio2.rb
@@ -0,0 +1,25 @@
+
+
+ciudades = %w['newyork' 'sanfrancisco' 'austin' 'seattle' 'whashington' ]
+
+puts "las ciudades por visitar son #{ciudades}"  
+
+if ciudades[1] = 'sanfrancisco'
+	ciudades[1]='sxnfrxncisco'
+end
+if ciudades[2] = 'austin'
+	ciudades[2] ='xustin'
+end
+if ciudades[3] ='seattle'
+	ciudades[3]='sexttle'
+end
+if ciudades[4]= 'whashington'
+	ciudades[4] ='whxshington'
+end	
+	
+
+ puts "ahora visite: #{ciudades}"
+ puts "las a remplazadas son: 5"
+ 
+
+
